57f1d78ada41101b8b9f91e7c170e22717f333f7
Matt Selensky <mselensky@gmail.com>
2022-08-05 00:09:46 -0500

    added conda channels

--- bngal-setup.sh
+++ bngal-setup.sh
@@ -3,26 +3,38 @@
 #### bngal setup script ####
 
 # 1. create conda environment and install system libraries
-conda create -n bngal -c conda-forge nlopt pandoc r=4.1 -y
+conda create -n bngal -y
+conda config --add channels r 
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda install -n bngal nlopt pandoc zlib r=4.1 r-fs r-rcpp r-igraph r-rcppeigen r-tidyverse r-rcolorbrewer r-visnetwork  -y
 source activate bngal
 
-# 2. export github directory as bngal variable for downstream scripts
+# 2. export github directory as bngal variable for downstream aliases
 export bngal=`pwd`
 
 # 3. add bngal Rscripts to user shell profiles
 # if macOS or linux, append to either zsh or bash shell:
+
 if [[ "$OSTYPE" == "darwin"* ]] || [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
-	if [[ "$SHELL" == "/bin/zsh" ]]; then
-		echo "alias bngal-build-nets=Rscript --vanilla ${bngal}/R/bngal-build-networks.R" >> ~/.zshrc
-	elif [[ "$SHELL" == "/bin/bash" ]]; then
-		echo "alias bngal-build-nets=Rscript --vanilla ${bngal}/R/bngal-build-networks.R" >> ~/.bashrc
+	alias_name="alias bngal-build-nets='Rscript --vanilla ${bngal}/R/bngal-build-networks.R'"
+
+	# output depends on zsh shell or bash
+	if [[ "$SHELL" == "/bin/zsh" && $(grep ${alias_name} ~/.zshrc | wc -l | xargs) == 0 ]]
+	then
+		echo ${alias_name} >> ~/.zshrc
+		source ~/.zshrc
+	elif [[ "$SHELL" == "/bin/bash" && $(grep ${alias_name} ~/.bashrc | wc -l | xargs) == 0 ]]
+	then
+		echo ${alias_name} >> ~/.bashrc
+		source ~/.bashrc
 	fi
+
 # if Windows, append to bash shell (cli tool not supported in other shells)
 elif [[ "$OSTYPE" == "win"* ]]; then
-
-	echo "alias bngal-build-nets=Rscript --vanilla ${bngal}/R/bngal-build-networks.R" >> C:\Program Files\Git\bin\bash.exe
-
+	windows_path="C:\Program Files\Git\bin\bash.exe"
+	echo "alias bngal-build-nets='Rscript --vanilla ${bngal}/R/bngal-build-networks.R'" >> $windows_path
 fi 
 
 # 4. install necessary R packages
