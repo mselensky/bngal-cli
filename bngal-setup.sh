2073e66739485c6ef4d8322af8e808a5f816845b
Matt Selensky <mselensky@gmail.com>
2023-01-09 14:29:54 -0600

    modified execs in shell profiles

--- bngal-setup.sh
+++ bngal-setup.sh
@@ -11,30 +11,30 @@ source activate bngal
 cp R/bngal-build-networks.R ${CONDA_PREFIX}/bin/bngal-build-networks.R
 cp R/bngal-summarize-networks.R ${CONDA_PREFIX}/bin/bngal-summarize-networks.R
 
-# save as aliases
-alias_name1="alias bngal-build-nets='Rscript --vanilla ${CONDA_PREFIX}/bin/bngal-build-networks.R'"
-alias_name2="alias bngal-summarize-nets='Rscript --vanilla ${CONDA_PREFIX}/bin/bngal-summarize-networks.R'"
+# save as functions
+function_name1="bngal-build-nets () { Rscript --vanilla ${CONDA_PREFIX}/bin/bngal-build-networks.R }"
+function_name2="bngal-summarize-nets () { Rscript --vanilla ${CONDA_PREFIX}/bin/bngal-summarize-networks.R }"
 
-# add bngal aliases to user shell profiles
+# add bngal functions to user shell profiles
 if [[ "$OSTYPE" == "darwin"* ]] || [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
 	# output depends on zsh shell or bash
-	if [[ "$SHELL" == "/bin/zsh" ]] && [[ $(grep -e "${alias_name1}" ~/.zshrc | wc -l | xargs) == 0 ]] && [[ $(grep -e "${alias_name2}" ~/.zshrc | wc -l | xargs) == 0 ]]
+	if [[ "$SHELL" == "/bin/zsh" ]] && [[ $(grep -e "${function_name1}" ~/.zshrc | wc -l | xargs) == 0 ]] && [[ $(grep -e "${function_name2}" ~/.zshrc | wc -l | xargs) == 0 ]]
 	then
-		echo ${alias_name1} >> ~/.zshrc
-		echo ${alias_name2} >> ~/.zshrc
+		echo ${function_name1} >> ~/.zshrc
+		echo ${function_name2} >> ~/.zshrc
 		source ~/.zshrc
-	elif [[ "$SHELL" == "/bin/bash" ]] && [[ $(grep -e "${alias_name1}" ~/.bashrc | wc -l | xargs) == 0 ]] && [[ $(grep -e "${alias_name2}" ~/.bashrc | wc -l | xargs) == 0 ]]
+	elif [[ "$SHELL" == "/bin/bash" ]] && [[ $(grep -e "${function_name1}" ~/.bashrc | wc -l | xargs) == 0 ]] && [[ $(grep -e "${function_name2}" ~/.bashrc | wc -l | xargs) == 0 ]]
 	then
-		echo ${alias_name1} >> ~/.bashrc
-		echo ${alias_name2} >> ~/.bashrc
+		echo ${function_name1} >> ~/.bashrc
+		echo ${function_name2} >> ~/.bashrc
 		source ~/.bashrc
 	fi
 # if Windows, append to bash shell (cli tool not supported in other shells)
 elif [[ "$OSTYPE" == "win"* ]] || [[ "$OSTYPE" == "msys" ]]; then
 	windows_path="C:\Program Files\Git\bin\bash.exe"
-	echo ${alias_name1} >> $windows_path
-	echo ${alias_name2} >> $windows_path
+	echo ${function_name1} >> $windows_path
+	echo ${function_name2} >> $windows_path
 
 else
 	echo "Error: OS not supported :("
